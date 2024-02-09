a49074884834cc72d46b1117917d0736ee06ffca
oscartena <oscar.tena.marin@gmail.com>
2024-02-09 16:00:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/practicatablelayout/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.practicatablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.appbartabs);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            Fragment fragment;
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        fragment = new Fragment1();
+                        break;
+                    case 1:
+                        fragment = new Fragment2();
+                        break;
+                    case 2:
+                        fragment = new Fragment3();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_fragment, fragment)
+                        .commit();
+                if (getSupportActionBar() != null) {
+                    getSupportActionBar().setTitle(tab.getText());
+                }
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+}
